3eb69162fe63a14692cd2be62620bfe6015b1fab
Rok Pajnič <Rok Pajni>
2016-12-06 20:31:29 +0100

    Floor textures added, skybox added
    Former-commit-id: 16518ab67c47504fb2bd2eafbdaa65e33d47fc19
    Former-commit-id: f807f2d8fa9e0549d845f8a570302405aed6770e

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 
 var canvas = document.getElementById('canvas');
-console.log(canvas);
+
 var engine = new BABYLON.Engine(canvas, true);
 
 
@@ -23,12 +23,31 @@ var initScene = function() {
 
     light.intensity = .5;
 
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+    
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 200.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/skybox/skybox", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.disableLighting = true;
+    skybox.material = skyboxMaterial;
 
+    console.log("floor init");
+    var floor = BABYLON.Mesh.CreateGround("floor", 10, 10, 2, scene);
+    var floorMaterial = new BABYLON.StandardMaterial("floorMat", scene);
+    floorMaterial.diffuseTexture = new BABYLON.Texture("assets/floor/Beech_01/VizPeople_Beech_1_Diffuse_2.jpg", scene);
+    floorMaterial.diffuseTexture.uScale = 2.0;//Repeat 5 times on the Vertical Axes
+    floorMaterial.diffuseTexture.vScale = 2.0;//Repeat 5 times on the Horizontal Axes
+    floorMaterial.specularColor = new BABYLON.Color3(0.5, 0.5, 0.5); // no ground reflection
+    //floorMaterial.bumpTexture = new BABYLON.Texture("assets/floor/hardwood_normal.jpg", scene);
+    floor.material = floorMaterial;
 
     // model loader
     var loader = new BABYLON.AssetsManager(scene);
 
+    BABYLON.OBJFileLoader.OPTIMIZE_WITH_UV = true;
     var model = loader.addMeshTask("model", "", "./assets/", "VRCEK.obj");
     model.onSuccess = function(promise) {
         promise.loadedMeshes.forEach(function(mesh) {
