7b9d7b9c50036cbf38abb289626cffb3a1aa5469
Rok Pajnič <Rok Pajni>
2017-02-23 14:22:03 +0100

    Camera fixes, walls with light added

--- js/app.js
+++ js/app.js
@@ -9,13 +9,15 @@ var initScene = function() {
     scene = new BABYLON.Scene(engine);
 
     
-    var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0, 0, 0, BABYLON.Vector3.Zero(), scene);
+    var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0, 0, 0, new BABYLON.Vector3(0, 1, 0), scene);
     camera.setPosition(new BABYLON.Vector3(0, 15, -10));
     camera.upperBetaLimit = 1.5;
-    camera.upperRadiusLimit = 12;
+    camera.upperRadiusLimit = 8;
     camera.lowerRadiusLimit = 3;
     camera.wheelPrecision = 50;
     camera.angularSensibilityX = 1000;
+    camera.inertia = 0.7
+    camera.panningSensibility = 0;;
     camera.attachControl(canvas, true, null); //null - dont move camera
 
 
@@ -24,10 +26,17 @@ var initScene = function() {
 
     // This creates a light, aiming 0,1,0 - to the sky.
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-    light.intensity = .5;
-
+    light.intensity = .8;
+    light.diffuse = new BABYLON.Color3(1, 1, 1);
+    light.specular = new BABYLON.Color3(1, 1, 1);
+    light.groundColor = new BABYLON.Color3(0, 0, 0);
     
+    var pointLight = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(0, 1, 0), scene);
+    pointLight.diffuse = new BABYLON.Color3(1, 1, 1);
+    pointLight.specular = new BABYLON.Color3(1, 1, 1);
+    pointLight.intensity = .8;
+
+
     var skybox = BABYLON.Mesh.CreateBox("skyBox", 200.0, scene);
     var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
     skyboxMaterial.backFaceCulling = false;
@@ -39,7 +48,7 @@ var initScene = function() {
     skybox.material = skyboxMaterial;
 
     console.log("floor init");
-    var floor = BABYLON.Mesh.CreateGround("floor", 10, 10, 2, scene);
+    var floor = BABYLON.Mesh.CreateGround("floor", 16, 16, 2, scene);
     floor.position.y = 0;
     floorMaterial = new BABYLON.StandardMaterial("floorMat", scene);
     floorMaterial.diffuseTexture = new BABYLON.Texture("assets/floor/Beech_01/Beech_01.jpg", scene);
@@ -50,6 +59,31 @@ var initScene = function() {
     floor.material = floorMaterial;
 
     //var box = BABYLON.Mesh.CreateSphere("sphere", 10.0, 1.0, scene, false,  BABYLON.Mesh.DEFAULTSIDE);
+
+    //walls
+    var wallN = BABYLON.MeshBuilder.CreatePlane("wallN", {size: 10, width: 16.0, height: 6, sideOrientation: 2}, scene);
+    wallN.position.y += 2.5;
+    wallN.position.z += 8;
+    var wallS = BABYLON.MeshBuilder.CreatePlane("wallS", {size: 10, width: 16.0, height: 6, sideOrientation: 2}, scene);
+    wallS.position.y += 2.5;
+    wallS.position.z -= 8;
+    var wallE = BABYLON.MeshBuilder.CreatePlane("wallE", {size: 10, width: 16.0, height: 6, sideOrientation: 2}, scene);
+    wallE.position.y += 2.5;
+    wallE.position.x += 8;
+    wallE.rotation.y = Math.PI/2;
+    var wallW = BABYLON.MeshBuilder.CreatePlane("wallW", {size: 10, width: 16.0, height: 6, sideOrientation: 2}, scene);
+    wallW.position.y += 2.5;
+    wallW.position.x -= 8;
+    wallW.rotation.y = Math.PI/2;
+
+    var walls = [wallN, wallS, wallE, wallW];
+    var wallMat = new BABYLON.StandardMaterial("wallMat", scene);
+    wallMat.alpha = 1;
+    wallMat.diffuseColor = new BABYLON.Color3(1 , 1, 1);
+
+    walls.forEach(function(wall) {
+        wall.material = wallMat;
+    }, this);
 
     BABYLON.OBJFileLoader.OPTIMIZE_WITH_UV = true;
 
